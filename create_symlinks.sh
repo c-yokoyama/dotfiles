c61dfeaef5e0d14aa275d672ee538940566c7dae
Chihiro Yokoyama <c.yokoyama@ntt.com>
2018-05-24 14:43:47 +0900

    add create_symlinks.sh

---
+++ create_symlinks.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ln -sf $(pwd)/tmux/.tmux.conf ~/.tmux.conf
+ln -sf $(pwd)/vim/.vimrc ~/.vimrc
+ln -sf $(pwd)/zsh/.zshenv ~/.zshenv
+ln -sf $(pwd)/zsh/.zshrc ~/.zshrc
+
+ln -sf $(pwd)/osx-plist/localhost.add-sshagent.plist ~/Library/LaunchAgents/localhost.add-sshagent.plist
+ln -sf $(pwd)/osx-plist/localhost.homebrew-upgrade.plist ~/Library/LaunchAgents/localhost.homebrew-upgrade.plist
