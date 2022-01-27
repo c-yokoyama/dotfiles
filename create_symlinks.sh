0d799cf794be4ef7305c4a1f78e15325ad58f65c
c-yokoyama <c_yokoyama@r.recruit.co.jp>
2022-01-28 00:34:47 +0900

    Remove unnessesary files

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-ln -sf $(pwd)/tmux/.tmux.conf ~/.tmux.conf
 ln -sf $(pwd)/vim/.vimrc ~/.vimrc
 ln -sf $(pwd)/zsh/.zshenv ~/.zshenv
 ln -sf $(pwd)/zsh/.zshrc ~/.zshrc
-ln -sf $(pwd)/zsh/.snippets ~/.snippets
-
-ln -sf $(pwd)/osx-plist/localhost.add-sshagent.plist ~/Library/LaunchAgents/localhost.add-sshagent.plist
-ln -sf $(pwd)/osx-plist/localhost.homebrew-upgrade.plist ~/Library/LaunchAgents/localhost.homebrew-upgrade.plis
-ln -sf $(pwd)/osx-plist/localhost.anyenv-update.plist ~/Library/LaunchAgents/localhost.anyenv-update.plist
